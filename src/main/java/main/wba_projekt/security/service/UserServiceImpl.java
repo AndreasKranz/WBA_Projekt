0aa05c450ab80798db8abd83f430e366234975f1
AndreasKranz <andreas.kranz.22.09@gmail.com>
2022-03-08 20:57:53 +0100

    annotations

--- src/main/java/main/wba_projekt/security/service/UserServiceImpl.java
+++ src/main/java/main/wba_projekt/security/service/UserServiceImpl.java
@@ -1,7 +1,11 @@
 package main.wba_projekt.security.service;
 
 import main.wba_projekt.task.model.Task;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
+@Service
 public class UserServiceImpl implements UserService{
 
 
